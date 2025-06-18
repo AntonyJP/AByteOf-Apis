b094764f760ff5b36334d7f71c696411b4d34dd1
ANTONY JOSTIN PEREZ PARRALES <antony.perez143@std.uni.edu.ni>
2025-06-18 13:09:13 -0600

    Agregar archivos de proyecto.

---
+++ Data/UsuarioContext.cs
@@ -0,0 +1,11 @@
+﻿using AByteOf熊猫Apis.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AByteOf熊猫Apis.Data
+{
+    public class UsuarioContext : DbContext
+    {
+        public DbSet<Usuarios> Usuarios { get; set; }
+        public UsuarioContext(DbContextOptions<UsuarioContext> options) : base(options) { }
+    }
+}
---
+++ Models/Usuarios.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AByteOf熊猫Apis.Models
+{
+    public class Usuarios
+    {
+        [Key]
+        public int IdUsuario { get; set; }
+        [Required, MaxLength(75)]
+        public string Nombre { get; set; }
+        [Required, EmailAddress]
+        public string Correo { get; set; }
+        [Required, MinLength(6)]
+        public string Contrasena { get; set; }
+    }
+}
